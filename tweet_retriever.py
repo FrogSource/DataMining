f42c6da08a80c893e757fce78cecebd3695e33ae
FrogSource <franziskabaethe@yahoo.de>
2020-02-26 22:25:51 +0100

    Implements first tweet retriever (WIP)

--- tweet_retriever.py
+++ tweet_retriever.py
@@ -1,6 +1,10 @@
 import urllib.request
 
-link = "https://api.twitter.com/1.1/search/tweets.json"
+key = "EWKjFPCPyXju9YmsMAtHVu5Nj"
+secret = "pX2Y22ZDzgxVn2NhVApZQQYAM0fcjh5C4v9cQH8sHLb3qMPZsr"
+access_token = "65090235-vTZHttfDUWh17HgJQyTqMq7JxIYbDvpJyuLqR3LQb"
+access_token_secret = "R7vhHiDDMPl3GJv5mvMD7OennabXmuTarZw0ap06nE8xd"
+link = "https://api.twitter.com/1.1/tweets/search/30day/tweets.json"
 query = "?q=Friedrich%20Merz"
 request = link+query
 print(request)
